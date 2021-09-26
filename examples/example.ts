a9d995a09f5120fff9cb262f0256fc9a9713404f
Yama <yamawaki00@gmail.com>
2021-09-26 20:24:07 +0900

    add: example

---
+++ examples/example.ts
@@ -0,0 +1,22 @@
+import { extractUrlFromSentence } from '../src'
+
+const example1 = `
+あいうえおウェブサイトhttps://a1.hogedomain.com/\n
+あいうえおwww.a2.hogedomain.comあいうえおhttps://a3.hogedomain.comかきくけこ
+メールアドレスb1@hogedomain.com\n
+めーるあどれすb2@hogedomain.comメールアドレスb3@hogedomain.comうぇぶさいとhttps://a4.hogedomain.comかきくけこ\n
+
+aiueo www.a5.hogedomain.com web site url\n
+aiueo https://a6.hogedomain.com email b4@hogedomain.com\n
+
+1. あいうえおhttps://a7.hogedomain.com かきくけこあいうえお a8.hogedomain.comかきくけこ\n
+2. あいうえおhttps://a9.hogedomain.comメールアドレスb5@hogedomain.comあいうえお\n
+
+a. aiueo https://a10.hogedomain.com?q=hoge&fuga=fuga1 aiueo\n
+b. b6@hogedomain.com
+
+🐱https://a11.hogedomain.com?q=hoge&fuga=fuga1🐈\n
+🐱b7@hogedomain.com🐈
+`
+
+extractUrlFromSentence(example1)
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+export const extractUrlFromSentence = (sentence: string): string[] =>
+{
+  const urlAndEmailRule: RegExp = /(?!.*<)([a-z0-9@/:-]{2,})([.]{1,})(.*)+[\w/]{2,}/gi
+  const emailRule: RegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/gi
+  const urlAndEmailPattern: RegExp = new RegExp(urlAndEmailRule)
+  const hasUrlText: boolean = urlAndEmailPattern.test(sentence)
+
+  if (!hasUrlText) {
+    return []
+  }
+
+  let duplecatedSentence: string = sentence.slice()
+
+  const urlAndEmailPatternText: RegExpMatchArray = duplecatedSentence.match(urlAndEmailPattern)
+
+  // remove multi byte string
+  const extractSingleByteText: string[] = urlAndEmailPatternText.map((text) => {
+    const duplicatedText = text.slice()
+    return duplicatedText.split(/[^\x01-\x7E]/gm)
+  })
+    .flat()
+    .filter((singleByteText) => singleByteText.length > 0)
+
+  // remove white space and email
+  const extractUrlText: string[] = extractSingleByteText.map((text) => {
+    const duplicatedText = text.slice()
+    return duplicatedText.split(/\s/gi)
+  })
+    .flat()
+    .filter((text) => {
+      const pattern = new RegExp(urlAndEmailRule)
+      return pattern.test(text)
+    })
+    .filter((text) => {
+      const emailPattern = new RegExp(emailRule)
+      return !emailPattern.test(text)
+    })
+  console.log(extractUrlText)
+  
+  return extractUrlText 
+}

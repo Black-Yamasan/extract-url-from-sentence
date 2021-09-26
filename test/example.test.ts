364077b2c5031052afc0c96f1e64f249cf26d65e
Yama <yamawaki00@gmail.com>
2021-09-26 21:03:41 +0900

    add: test

---
+++ test/example.test.ts
@@ -0,0 +1,19 @@
+import { extractUrlFromSentence } from '../src/index'
+
+const sampleText1 = `
+URL1つ目: https://a1.hogehoge.hoge
+URL2つ目と3つ目:  https://a2.hogehoge.hogeウェブサイト https://a3.hogehoge.hoge
+メールアドレス: b1@hogehoge.hoge
+メールアドレスと4つ目のURL: b2@hogehoge.hoge web site url a4.hogehoge.hoge
+`
+
+describe('extractUrlFromSentence test', () => {
+
+  test('文章中に含まれるURLの数をテスト', () => {
+    expect(extractUrlFromSentence(sampleText1).length === 4)
+  })
+
+  test('先頭のウェブサイトURLをテスト', () => {
+    expect(extractUrlFromSentence(sampleText1)[0] === 'https://a1.hogehoge.hoge')
+  })
+})
